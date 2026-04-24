0634523251b2a155dc31e051b28c2ce38f3fb3b8
alfhz <alfh.zachra@gmail.com>
2026-04-24 15:56:37 +0700

    initial proyek

---
+++ W9/p2p-lending-app/src/test/java/com/p2p/AppTest.java
@@ -0,0 +1,13 @@
+package com.p2p;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        // Ini adalah satu-satunya bagian penting dari kode aslimu
+        assertTrue(true);
+    }
+}
